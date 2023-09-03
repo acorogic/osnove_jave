526c36b4c9e55908c20149851ae6f11fc0efa046
Aleksandar R <acorogic@gmail.com>
2023-09-03 21:12:52 +0200

    18.8.2023 zadaci za vezbanje

--- src/main/java/p08_18_2023/Zadtatak10.java
+++ src/main/java/p08_18_2023/Zadtatak10.java
@@ -1,6 +1,11 @@
 package p08_18_2023;
 
+import java.util.Scanner;
+
 public class Zadtatak10 {
+    public static void main(String[] args) {
+
+
 
 //    (Za vezbanje)
 //    Napisati program koji stampa 20 pozicija.
@@ -13,4 +18,20 @@ public class Zadtatak10 {
 //            * * * * _ _ _ _ _ _ * * * * * * * * * *
 //    Objasnjenje: stampamo _ od 5 do 10 u ostalim situacijama stampamo *
 
-}
+        Scanner s = new Scanner(System.in);
+        System.out.println("unesite a");
+        int a= s.nextInt();
+
+        System.out.println("unesite b");
+        int b= s.nextInt();
+
+        for (int i = 1; i <= 20; i++) {
+
+            if (i>=a && i<=b) {
+                System.out.print(" _ ");}
+            else {
+                System.out.print(" * ");
+
+        }
+
+}}}
